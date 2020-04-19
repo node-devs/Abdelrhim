355d7c2ed9f6dd24965ef01ca2e4f237195442a7
abdelrhim99 <abdelrhim999952@gmail.com>
2020-04-19 18:16:27 +0200

    update

--- app.js
+++ app.js
@@ -8,14 +8,4 @@ app.get("/members", function(req,res){
 })
 
 app.listen(6547)
-console.log("Hello")const express = require ("express")
-const app = express()
-
-app.get("/members", function(req,res){
-    var members = [{name: "Siddig"}, {name:"Abdelrhim"}, {name:"Omar"}]
-    res.send(members)
-
-})
-
-app.listen(6547)
 console.log("Hello")
